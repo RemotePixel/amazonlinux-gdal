7e38820548e02f61e6a8446ca8ee8f00eae17da1
vincentsarago <vincent.sarago@gmail.com>
2019-08-27 09:52:24 +0300

    update

--- scripts/test-layer.sh
+++ scripts/test-layer.sh
@@ -8,11 +8,12 @@ PYTHON_VERSION=$2
 docker run \
     --name lambda \
     --volume $(pwd)/:/local \
-    --env GDAL_DATA=/opt/lib \
+    --env GDAL_DATA=/opt/share/gdal \
+    --env PROJ_LIB=/opt/share/proj \
     --env GDAL_VERSION=${GDAL_VERSION} \
     --env PYTHON_VERSION=${PYTHON_VERSION} \
     --env PYTHONPATH=/opt/python:/var/runtime \
-    -itd lambci/lambda:build-python3.6 bash
+    -itd lambci/lambda:build-python${PYTHON_VERSION} bash
 docker cp ./layer-gdal${GDAL_VERSION}-py${PYTHON_VERSION}.zip lambda:/tmp/layer-gdal${GDAL_VERSION}-py${PYTHON_VERSION}.zip
 docker exec -it lambda bash -c 'unzip -q /tmp/layer-gdal${GDAL_VERSION}-py${PYTHON_VERSION}.zip -d /opt/'
 docker exec -it lambda python -c 'import rasterio; src = rasterio.open("https://oin-hotosm.s3.amazonaws.com/5ac626e091b5310010e0d482/0/5ac626e091b5310010e0d483.tif"); print(src.meta)'
